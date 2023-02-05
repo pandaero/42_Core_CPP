83b471b2715e3c7ed1ddc4700c78866dc5240330
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-02-05 15:43:58 +0100

    first exercise with makefile and code

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+
+//Function to check for lowercase characters and make them uppercase. Return same otherwise.
+char	upperwhenlower(char ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+		return (ch + 'A' - 'a');
+	return (ch);
+}
+
+//Function turns a whole string to uppercase until null.
+std::string	*stringupperwhenlower(char *str)
+{
+	int	i = 0;
+	std::string *	out = new std::string();
+
+	while (str[i] != '\0')
+	{
+		out->push_back(upperwhenlower(str[i]));
+		i++;
+	}
+	return (out);
+}
+
+//Program takes string arguments and rewrites them out in uppercase.
+int	main(int argc, char **argv)
+{
+	int				i = 1;
+	std::string *	str = NULL;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (argc > 1 && i < argc)
+	{
+		str = stringupperwhenlower(argv[i]);
+		std::cout << *str;
+		delete str;
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
