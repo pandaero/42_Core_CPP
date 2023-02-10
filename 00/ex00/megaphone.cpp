a265b30f767503ab501e2a743c02c3dd8e7db4ea
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-02-10 13:52:25 +0100

    shortened and used more c++ for megaphone

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,48 +6,30 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 22:41:27 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/02/05 22:41:29 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/02/10 13:51:16 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstring>
-
-//Function to check for lowercase characters and make them uppercase. Return same otherwise.
-char	upperwhenlower(char ch)
-{
-	if (ch >= 'a' && ch <= 'z')
-		return (ch + 'A' - 'a');
-	return (ch);
-}
-
-//Function turns a whole string to uppercase until null.
-std::string	*stringupperwhenlower(char *str)
-{
-	int				i = 0;
-	std::string *	out = new std::string();
-
-	while (str[i] != '\0')
-	{
-		out->push_back(upperwhenlower(str[i]));
-		i++;
-	}
-	return (out);
-}
+#include <string>
 
 //Program takes string arguments and rewrites them out in uppercase.
 int	main(int argc, char **argv)
 {
 	int				i = 1;
-	std::string *	str = NULL;
+	char			ch;
+	std::string 	in, str;
 
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	while (argc > 1 && i < argc)
 	{
-		str = stringupperwhenlower(argv[i]);
-		std::cout << *str;
-		delete str;
+		in = argv[i];
+		for (int j = 0; j < (int) in.length(); j++)
+		{
+			ch = toupper(in[j]);
+			std::cout << ch;
+		}
 		i++;
 	}
 	std::cout << std::endl;
