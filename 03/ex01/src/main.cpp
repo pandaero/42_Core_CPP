807059cba4136d0ec5844c67f77a94a25afba16c
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2023-02-17 08:58:02 +0100

    added a Claptrap to test

--- 03/ex01/src/main.cpp
+++ 03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:44:25 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/02/17 08:52:54 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/02/17 08:56:44 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,11 @@ int	main(void)
 	Fred.guardGate();
 	// Fred.beRepaired(50);
 	// Fred.takeDamage(75);
+	std::cout << "----- ----- ----- ----- ----- -----" << std::endl;
+	ClapTrap	Louis("Louis");
+	Louis.takeDamage(10);
+	Louis.beRepaired(5);
+	Louis.attack("Sally");
 	std::cout << "----- ----- ----- ----- ----- -----" << std::endl;
 	return (0);
 }
