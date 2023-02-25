4cc490a1dff97286826faf57bef30792d8217a01
pandalaf <pandalaf@student.42wolfsburg.de>
2023-02-25 17:01:46 +0100

    working exercise cpp 04 ex 01

--- 04/ex01/src/main.cpp
+++ 04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 20:12:32 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/02/22 16:32:03 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/02/25 16:54:45 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,46 +23,33 @@ int	main(void)
 	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
 	
 	{
-		Animal	array[4];
+		const Animal *	array[4];
 
-		Dog		doggo;
-		Dog		doggy;
-		Cat		catto;
-		Cat		catty;
+		for (int i = 0; i < 4; i++)
+		{
+			if (i % 2 == 0)
+				array[i] = new Dog();
+			else
+				array[i] = new Cat();	
+		}
 
-		array[0] = doggo;
-		array[1] = doggy;
-		array[2] = catto;
-		array[3] = catty;
+		for (int i = 0; i < 4; i++)
+			array[i]->makeSound();
 
-		array[0].makeSound();
-		array[1].makeSound();
-		array[2].makeSound();
-		array[3].makeSound();
+		for (int i = 0; i < 4; i++)
+			delete array[i];
 	}
 
 	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
 
-
-
-	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
-
 	{
-
 		Brain	brain;
 		Brain	two(brain);
 		Brain	three;
 
 		three = brain;
-
 	}
-	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
 	
-	// for (int i = 0; i < 4; i++)
-	// {
-	// 	delete &array[i];
-	// }
-
 	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
 	
 	{
@@ -71,9 +58,9 @@ int	main(void)
 
 		j->makeSound();
 		i->makeSound();
-		
-		delete	j;
-		// delete	i;
+
+		delete j;
+		delete i;
 	}
 	
 	std::cout << "----- ----- ----- ----- ----- ----- ----- ----- ----- -----" << std::endl;
