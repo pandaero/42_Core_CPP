b467b58f6dc807762bdd926c252199166164c4eb
pandalaf <pandalaf@student.42wolfsburg.de>
2023-03-05 23:20:55 +0100

    added test for exception

--- 07/ex02/src/main.cpp
+++ 07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 14:22:39 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/03/05 23:13:47 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/03/05 23:20:41 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,15 @@ int	main()
 		for (int i = 0; i < (int) arr2.size(); i++)
 			std::cout << " " << arr2[i];
 		std::cout << std::endl;
+		
+		try
+		{
+			arr2[80];
+		}
+		catch (std::exception & exc)
+		{
+			std::cerr << exc.what() << std::endl;
+		}
 	}
 
 	std::cout << "----- ----- ----- -----" << std::endl;
