99bcd2c09d0742d9ee391b48b3b2ebd07fc97018
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2023-03-21 00:39:18 +0100

    Âmoved printing to right after operations complete

--- 09/ex02/src/main.cpp
+++ 09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 11:10:16 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/03/20 15:38:29 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/03/21 00:38:02 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,16 @@ int	main(int argc, char **argv)
 	}
 	clock_t	diffVector = clock() - refVector;
 
+	std::cout << "After : ";
+	for (size_t i = 0; i < cont.size(); ++i)
+	{
+		std::cout << cont[i];
+		if (i != cont.size() - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Time to process range of " << argc - 1 << " elements with std::vector : " << std::setprecision(1) << std::fixed << 1000000 * ((float) diffVector) / CLOCKS_PER_SEC << " us" << std::endl;
+
 	clock_t	refList = clock();
 	try
 	{
@@ -71,16 +81,6 @@ int	main(int argc, char **argv)
 	}
 	clock_t	diffList = clock() - refList;
 
-	std::cout << "After : ";
-	for (size_t i = 0; i < cont.size(); ++i)
-	{
-		std::cout << cont[i];
-		if (i != cont.size() - 1)
-			std::cout << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "Time to process range of " << argc - 1 << " elements with std::vector : " << std::setprecision(1) << std::fixed << 1000000 * ((float) diffVector) / CLOCKS_PER_SEC << " us" << std::endl;
 	std::cout << "Time to process range of " << argc - 1 << " elements with std::list   : " << std::setprecision(1) << std::fixed << 1000000 * ((float) diffList) / CLOCKS_PER_SEC << " us" << std::endl;
 	return (0);
 }
